a48de5a017c1ed6b2cf90ecb321362805833946c
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-03-27 11:38:12 +0300

    Add files via upload

---
+++ Console2703Dz/Console2703Dz2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2703Dz2
+{
+    class Program
+    {
+        //Реализовать инвентарь.
+
+        static void Main(string[] args)
+        {
+            List<string> numbers = new List<string>();
+
+            while (true)
+            {
+                Console.WriteLine("1)Показать содержимое рюкзака\n2)Добавить предмет в рюкзак\n3)Удалить предмет из рюкзака\n4)Выйти из программы");
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 1:
+                        foreach (string i in numbers)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        break;
+                    case 2:
+                        string addItem = Console.ReadLine();
+                        numbers.Add(addItem);
+                        break;
+                    case 3:
+                        string removeItem = Console.ReadLine();
+                        numbers.Remove(removeItem);
+                        break;
+                }
+
+                if(answer == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Console2703Dz/Console2703Dz3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2703Dz3
+{
+    class Program
+    {
+        // Пользователь вводит числа, и программа их запоминает.
+        // Как только пользователь введёт команду sum, программа
+        // выведет сумму всех веденных чисел.
+        // Выход из программы должен происходить только
+        // в том случае, если пользователь введет команду exit.
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int sum = 0;
+
+            while (true)
+            {
+                string answer = Console.ReadLine();
+                
+
+                if (answer == "exit")
+                {
+                    break;
+                }
+                if (answer == "sum")
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    numbers.Add(Convert.ToInt32(answer));
+                }
+            }
+        }
+    }
+}
