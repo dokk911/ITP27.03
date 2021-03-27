0da55fa2c6980a470c103a456d34f8848b6ff304
dokk911 <76562544+dokk911@users.noreply.github.com>
2021-03-27 12:49:46 +0300

    Add files via upload

---
+++ Console2703Dz/Console2703Dz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2703Dz1
+{
+    class Program
+    {
+        //Посчитать сумму элементов списка.
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            Random random = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Console2703Dz/Console2703Dz3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console2703Dz3
+{
+    class Program
+    {
+        // Пользователь вводит числа, и программа их запоминает.
+        // Как только пользователь введёт команду sum, программа
+        // выведет сумму всех веденных чисел.
+        // Выход из программы должен происходить только
+        // в том случае, если пользователь введет команду exit.
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            bool isProgramStop = false;
+            int sum = 0;
+
+            while (isProgramStop == false)
+            {
+                string answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "exit":
+                        isProgramStop = true;
+                        break;
+                    case "sum":
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+
+                        Console.WriteLine(sum);
+                        break;
+                    default:
+                        int number;
+                        int.TryParse(answer, out number);
+                        numbers.Add(number);
+                        break;
+                }
+            }
+        }
+    }
+}
